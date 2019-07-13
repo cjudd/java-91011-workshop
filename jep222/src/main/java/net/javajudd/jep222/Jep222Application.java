7ec21cc14997be723e5edc7505d60007df8af72d
Christopher M. Judd <javajudd@gmail.com>
2019-07-13 17:35:07 -0400

    Generated jep222 project from Spring Initializer.

---
+++ jep222/src/main/java/net/javajudd/jep222/Jep222Application.java
@@ -0,0 +1,13 @@
+package net.javajudd.jep222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jep222Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jep222Application.class, args);
+	}
+
+}
---
+++ jep222/src/test/java/net/javajudd/jep222/Jep222ApplicationTests.java
@@ -0,0 +1,16 @@
+package net.javajudd.jep222;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jep222ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
