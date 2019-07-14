c25747362be417c52cda852e626ddc5d20747ade
Christopher M. Judd <javajudd@gmail.com>
2019-07-14 19:03:56 -0400

    Generated jep224 project from Spring Initializer.

---
+++ jep224/src/main/java/net/javajudd/jep224/Jep224Application.java
@@ -0,0 +1,13 @@
+package net.javajudd.jep224;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jep224Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jep224Application.class, args);
+	}
+
+}
---
+++ jep224/src/test/java/net/javajudd/jep224/Jep224ApplicationTests.java
@@ -0,0 +1,16 @@
+package net.javajudd.jep224;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Jep224ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
