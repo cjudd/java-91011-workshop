dbdc35eccd08737c0452b7ce0ebe2063fcea47ee
Christopher M. Judd <javajudd@gmail.com>
2019-07-15 10:20:50 -0600

    Added JEP 286 module using Spring Initializer web application.

---
+++ jep286/src/main/java/net/javajudd/jep286/Jep286Application.java
@@ -0,0 +1,13 @@
+package net.javajudd.jep286;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jep286Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jep286Application.class, args);
+	}
+
+}
